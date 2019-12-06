9d5251e61a7464e931931fa553311908f0a6f7a4
lxl <lxl177177@163.com>
2019-12-06 09:37:45 +0800

    Initial commit

---
+++ src/main/java/Base/App.java
@@ -0,0 +1,19 @@
+package Base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void fun1(){
+        System.out.println("dddd");
+    }
+}
+
+
