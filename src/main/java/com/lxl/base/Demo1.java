546d58c1c289d2ea2a002b4446a00c0e2c334f63
lxl <lxl177177@163.com>
2019-12-21 16:25:54 +0800

    1.测试新项目提交

---
+++ src/main/java/com/lxl/base/Demo1.java
@@ -0,0 +1,15 @@
+package com.lxl.base;
+
+/**
+ * ClassName Demo1
+ *
+ * @Author Administrator
+ * @Date 2019/12/6  13:45
+ * Version 1.0
+ **/
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("ccc");
+    }
+}
