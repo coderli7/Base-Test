91938ba38b28d8b633bad2a8354040d144d7f4c4
lxl <lxl177177@163.com>
2019-12-25 10:34:22 +0800

    1.修改项目结构

---
+++ Base-ArrAndCol/src/main/java/com/lxl/arrandcol/Demo1.java
@@ -0,0 +1,27 @@
+package com.lxl.arrandcol;
+
+import java.util.LinkedList;
+
+/**
+ * ClassName Demo1
+ *
+ * @Author Administrator
+ * @Date 2019/12/25  10:19
+ * Version 1.0
+ **/
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+
+        LinkedList linkedList=new LinkedList();
+        linkedList.add("a");
+        linkedList.add("b");
+        linkedList.add("c");
+        linkedList.add("d");
+        Object a = linkedList.get(0);
+        linkedList.remove(2);
+        System.out.println(a);
+        System.out.println(linkedList);
+    }
+}
