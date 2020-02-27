0af058cef4a1d728c6c82d89f029c8074a6fdedc
lxl <lxl177177@163.com>
2020-02-27 23:04:20 +0800

    1.all1

---
+++ Base-ArrAndCol/src/main/java/com/lxl/models/entity/User.java
@@ -0,0 +1,45 @@
+package com.lxl.models.entity;
+
+import java.io.Serializable;
+
+/**
+ * (User)实体类
+ *
+ * @author makejava
+ * @since 2020-02-18 18:01:07
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 515273293312415276L;
+    
+    private Integer id;
+    
+    private String name;
+    
+    private Integer age;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
