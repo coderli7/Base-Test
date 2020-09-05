e263f0d329314b91abb5cb8944caf9fd6d2e72eb
lxl <lxl177177@163.com>
2020-09-05 15:19:55 +0800

    test

---
+++ GeneratorSqlmapCustom/src/main/java/GeneratorSqlmap.java
@@ -0,0 +1,52 @@
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+            System.out.println("OK!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        // 指定 逆向工程配置文件
+
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL url = classLoader.getResource("generatorConfig.xml");
+        String path1 = url.getPath();
+        System.out.println(path1);
+
+
+/*//文件的实际路径
+		String realPath = url.getPath();
+
+		InputStream in = new FileInputStream(realPath);
+
+		InputStreamReader reader = new InputStreamReader(in, "UTF-8");*/
+
+
+        File configFile = new File(path1);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+}
